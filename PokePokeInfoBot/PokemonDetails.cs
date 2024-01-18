fe7befbbbcaa6241253014b1f17dec48aead394b
YEGCSharpDev <89283498+YEGCSharpDev@users.noreply.github.com>
2024-01-17 21:28:41 -0700

    Modified code to use PokeApiNet Nuget package

---
+++ PokePokeInfoBot/PokemonDetails.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using PokeApiNet;
+using System.Reflection;
+
+namespace PokePokeInfoBot
+{
+    internal class PokemonDetails
+    {
+        PokeApiClient pokeClient = new PokeApiClient();
+
+        public async Task <Pokemon> GetThisPokemon(string pokemonName)
+        {
+
+            return await pokeClient.GetResourceAsync<Pokemon>(pokemonName);
+        }
+
+        public async Task<PokeApiNet.Type> GetThisType(string pokeURI)
+        {
+            var urlElements = pokeURI.Split('/');
+            var a = urlElements[(urlElements.Length) - 2];
+            try
+            {
+                return await pokeClient.GetResourceAsync<PokeApiNet.Type>(int.Parse(urlElements[(urlElements.Length)-2]));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+
+
+    }
+}
--- PokePokeInfoBot/Program.cs
+++ PokePokeInfoBot/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using PokeApiNet;
+using PokePokeInfoBot;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Exceptions;
@@ -54,9 +58,18 @@ class Program
 
             string input = message.Text.ToLower();
 
-            await FetchPokemonInfo(message.Chat.Id, input);
-  
+            if (input.Contains("/start"))
+            {
+            Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: message.Chat.Id,
+            text: "Hello \nPlease Enter just the pokemon name to get its details" ,
+            cancellationToken: cancellationToken);
 
+            }
+        else
+        {
+            await FetchPokemonInfo(message.Chat.Id, input);
+        }
     }
     static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
     {
@@ -74,77 +87,80 @@ class Program
 
     private static async Task FetchPokemonInfo(long chatId, string pokemonName)
     {
-        string apiUrl = $"https://pokeapi.co/api/v2/pokemon/{pokemonName}";
-        HttpClient client = new HttpClient();
-
         try
         {
-            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            
 
-            if (response.IsSuccessStatusCode)
-            {
-                string json = await response.Content.ReadAsStringAsync();
-                PokemonData pokemon = JsonConvert.DeserializeObject<PokemonData>(json);
+                PokemonDetails creatureDetails = new PokemonDetails();
+                var pokemon = creatureDetails.GetThisPokemon(pokemonName).Result;
 
-                string message = $"Name: {pokemon.Name}\n" +
-                                 $"Weight: {pokemon.Weight}\n" +
-                                 $"Height: {pokemon.Height}\n\n" +
-                                 "Abilities:\n";
-                foreach (var ability in pokemon.Abilities)
-                {
-                    message += $"- {ability.Ability.Name}\n";
-                }
+            string message = $"Name: {pokemon.Name}\n" +
+                             $"Weight: {pokemon.Weight}\n" +
+                             $"Height: {pokemon.Height}\n\n";
 
-                message += "\nBase Stats:\n";
-                foreach (var stat in pokemon.Stats)
-                {
-                    message += $"- {stat.Stat.Name}: {stat.BaseStat}\n";
-                }
+            message += "Abilities:\n" + GetAbilitiesMessage(pokemon.Abilities) + "\n";
+           // message += "Base Stats:\n" + GetStatsMessage(pokemon.Stats) + "\n";
+            message += "Type: " + GetTypeMessage(pokemon.Types,creatureDetails);
 
-                await botClient.SendTextMessageAsync(chatId, message);
-            }
-            else
-            {
-                await botClient.SendTextMessageAsync(chatId, $"Failed to fetch data. Status code: {response.StatusCode}");
-            }
+
+            await botClient.SendTextMessageAsync(chatId, message);
+
         }
         catch (Exception ex)
         {
             await botClient.SendTextMessageAsync(chatId, $"An error occurred: {ex.Message}");
         }
-        finally
+
+    }
+
+    // Extracted methods for better modularity
+
+    private static string GetAbilitiesMessage(List<PokemonAbility> abilities)
+    {
+        return string.Join("\n", abilities.Select(ability => $"- {ability.Ability.Name}"));
+    }
+
+    private static string GetStatsMessage(List<PokemonStat> stats)
+    {
+        return string.Join("\n", stats.Select(stat => $"- {stat.Stat.Name}: {stat.BaseStat}"));
+    }
+
+    private static string GetTypeMessage(List<PokemonType> types, PokemonDetails creatureDetails)
+    {
+        var message = new StringBuilder();
+
+        foreach (var type in types)
         {
-            client.Dispose();
+            var typeDetails = creatureDetails.GetThisType(type.Type.Url).Result;
+            message.AppendLine($"{type.Type.Name}");
+
+            var damageRelations = typeDetails.DamageRelations;
+
+            AppendDamageRelations(message, "Strong against (Takes No DMG)", damageRelations.NoDamageFrom);
+            AppendDamageRelations(message, "Super Effective against (Deals Double DMG)", damageRelations.DoubleDamageTo);
+            AppendDamageRelations(message, "Effective against (Half DMG to)", damageRelations.HalfDamageTo);
+            AppendDamageRelations(message, "Ineffective Against (No DMG)", damageRelations.NoDamageTo);
+            AppendDamageRelations(message, "Weak against (Half DMG From)", damageRelations.HalfDamageFrom);
+            AppendDamageRelations(message, "Very Weak against (Double DMG From", damageRelations.DoubleDamageFrom);
+
+        }
+
+        return message.ToString();
+    }
+
+    private static void AppendDamageRelations(StringBuilder message, string label, List<NamedApiResource<PokeApiNet.Type>> relations)
+    {
+        if (relations.Count > 0)
+        {
+            message.AppendLine($"- {label}:");
+            foreach (var relation in relations)
+            {
+                message.AppendLine($"  - {relation.Name}");
+            }
         }
     }
 }
 
-public class PokemonData
-{
-    public string Name { get; set; }
-    public int Weight { get; set; }
-    public int Height { get; set; }
-    public List<AbilityInfo> Abilities { get; set; }
-    public List<StatInfo> Stats { get; set; }
-}
 
-public class AbilityInfo
-{
-    public Ability Ability { get; set; }
-}
 
-public class Ability
-{
-    public string Name { get; set; }
-}
 
-public class StatInfo
-{
-    public int BaseStat { get; set; }
-    public Stat Stat { get; set; }
-}
-
-public class Stat
-{
-    public string Name { get; set; }
-}
